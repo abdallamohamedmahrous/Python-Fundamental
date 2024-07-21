e1949f4a3cffbde0d00f48a27f7121b451dcd924
Abdalla <abdallamahrous1221@gmail.com>
2024-07-22 01:54:45 +0300

    Solve problem 2

---
+++ Problem_Solving/Basic_Data_Types  .py
@@ -0,0 +1,10 @@
+input_line = input()
+values = input_line.split()
+
+int_val = int(values[0])
+long_long_val = int(values[1])  
+char_val = values[2]           
+float_val = float(values[3])
+double_val = float(values[4])   
+print(int_val),print(long_long_val),print(char_val),print(f"{float_val:.6f}"),print(double_val) 
+
