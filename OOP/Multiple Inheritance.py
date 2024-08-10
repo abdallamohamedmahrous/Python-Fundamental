7c089c9d5d06d8cde56d4b8e8f2c3261e19b49af
Abdalla <abdallamahrous1221@gmail.com>
2024-08-10 19:03:35 +0300

    Solve Task_12 in OOP

---
+++ OOP/Multiple Inheritance.py
@@ -0,0 +1,62 @@
+class Person:
+    def __init__(self, name, age) -> None:
+        self.__name = name
+        self.__age = age
+
+    def displayInfo(self):
+        print(f"My Name Is: {self.__name}\nMy Age Is: {self.__age}")
+
+class Worker:
+    def __init__(self, workerID, role) -> None:
+        self.__workerID = workerID
+        self.__role = role
+
+    def displayWorkerInfo(self):
+        print(f"My Worker ID is {self.__workerID}\nI'm {self.__role}")
+
+class Employee(Person, Worker):
+    def __init__(self, name, age, workerID, role, employeeID, department) -> None:
+        Person.__init__(self, name, age)
+        Worker.__init__(self, workerID, role)
+        self.__employeeID = employeeID
+        self.__department = department
+
+    def showDepartment(self):
+        print(f"I Work In department: {self.__department}")    
+
+    def displayInfo(self):
+        Person.displayInfo(self)
+        Worker.displayWorkerInfo(self)
+        print(f"The Employee ID is: {self.__employeeID}\nI Work In department: {self.__department}")
+
+class Manager(Employee):
+    def __init__(self, name, age, workerID, role, employeeID, department, numTeams) -> None:
+        super().__init__(name, age, workerID, role, employeeID, department)
+        self.__numTeams = numTeams
+
+    def displayInfo(self):
+        super().displayInfo()   
+        print(f"Number Of Teams: {self.__numTeams}") 
+   
+    def showTeams(self):
+        print(f"Number Of Teams: {self.__numTeams}")         
+
+class Engineer(Employee): 
+    def __init__(self, name, age, workerID, role, employeeID, department, specialization) -> None:
+        super().__init__(name, age, workerID, role, employeeID, department)
+        self.__specialization = specialization
+
+    def showspecialization(self):
+        print(f"The specialization is: {self.__specialization}")
+
+    def displayInfo(self):
+        super().displayInfo()
+        print(f"The specialization is: {self.__specialization}") 
+
+Manager1=Manager(name=input("Enter Your name: "),age=input("Enter Your Age: "),workerID=input("Enter Work ID: "),role=input("What is your Role: "),employeeID=input("Enter Your ID: "),department=input("Enter your department: "),numTeams=input("Enter Number Of Team You Have: "))
+print("\nThe Manger Info:")
+Manager1.displayInfo()
+print("__"*40)
+Engineer1=Engineer(name=input("Enter Your name: "),age=input("Enter Your Age: "),workerID=input("Enter Work ID: "),role=input("What is your Role: "),employeeID=input("Enter Your ID: "),department=input("Enter your department: "),specialization=input("Enter your specialization: "),)
+print("\nThe Enginner Info:")
+Engineer1.displayInfo()
